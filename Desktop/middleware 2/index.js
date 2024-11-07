24993131eb3252012bca2c1d3d8d883d8007153b
Pavan <reddypavankumar709@gmail.com>
2024-11-07 17:39:15 +0530

    These the first lesson of the middleware

---
+++ Desktop/middleware 2/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+
+const app = express();
+// log the method ,timestamp and the url
+
+function loggermiddleware(req,res,next){
+    console.log("method is"+req.method);
+    console,log("Host is"+ req.url);
+    console.log(new Date());
+    next();
+}
+
+app.get("/sum", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.use(loggermiddleware);
+
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a / b
+    })
+
+});
+
+app.get("/subtract", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans: a - b
+    })
+});
+
+app.listen(3000);
+console.log("successful entered into port")
